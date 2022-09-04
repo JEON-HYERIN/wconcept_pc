5a88b312608306e1b9e3f5d8b843af2736895a42
JEON-HYERIN <jeonhyelin19951118@gmail.com>
2022-09-04 23:42:27 +0900

    0904

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,4 +1,32 @@
 $(function(){
+  const body = document.querySelector('body');
+  let scrollPosition = 0;
+  
+  enable();
+
+  // page-load
+  $('.page-load, body').click(function(){
+    disable();
+    $('.page-load').css('display', 'none');
+  });
+
+  // body fixed
+  function enable() {
+    scrollPosition = window.pageYOffset;
+    body.style.overflow = 'hidden';
+    body.style.position = 'fixed';
+    body.style.top = `-${scrollPosition}px`;
+    body.style.width = '100%';
+  }
+
+  function disable() {
+    body.style.removeProperty('overflow');
+    body.style.removeProperty('position');
+    body.style.removeProperty('top');
+    body.style.removeProperty('width');
+    window.scrollTo(0, scrollPosition);
+  }
+
   // gnb
   $('.gnb-item').mouseenter(function(){
     $(this).children('.lnb-list-wrap').stop().fadeIn(300);
