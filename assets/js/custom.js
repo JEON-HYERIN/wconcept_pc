4245d1253a61f304f50574abbbfc1b7fc2179e48
JEON-HYERIN <jeonhyelin19951118@gmail.com>
2022-09-09 08:18:58 +0900

    0909

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,31 +1,8 @@
 $(function(){
-  const body = document.querySelector('body');
-  let scrollPosition = 0;
-  
-  enable();
-
-  // page-load
-  $('.page-load, body').click(function(){
-    disable();
-    $('.page-load').css('display', 'none');
+  // a태그 기본동작 방지
+  $(document).on('click', 'a[href="#"]', function (e) {
+    e.preventDefault();
   });
-
-  // body fixed
-  function enable() {
-    scrollPosition = window.pageYOffset;
-    body.style.overflow = 'hidden';
-    body.style.position = 'fixed';
-    body.style.top = `-${scrollPosition}px`;
-    body.style.width = '100%';
-  }
-
-  function disable() {
-    body.style.removeProperty('overflow');
-    body.style.removeProperty('position');
-    body.style.removeProperty('top');
-    body.style.removeProperty('width');
-    window.scrollTo(0, scrollPosition);
-  }
 
   // gnb
   $('.gnb-item').mouseenter(function(){
